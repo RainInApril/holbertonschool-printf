503ad6397413d1388108b6026366e23ce5adbaf8
root <5051@holbertonschool.com>
2022-07-07 03:47:53 -0700

    Add protos.

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-typedef struct printf
+typedef struct modifier
+{
+	char *symbol;
+	int (*func)(va_list list);
+} s_mod;
 
-char *print_i(char *format, va_list list);
+int print_i(va_list list);
+int print_c(va_list list);
 
 #endif /* _MAIN_H_ */
