01bde80225a7c6bd3037e1df7010821edbfa48b6
root <5051@holbertonschool.com>
2022-07-07 04:18:17 -0700

    Add proto _strlen _strdup _strcpy.

--- main.h
+++ main.h
@@ -11,6 +11,9 @@ typedef struct modifier
 int print_i(va_list list);
 int print_c(va_list list);
 int print_s(va_list list);
-
+int _putchar(char c);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif /* _MAIN_H_ */
