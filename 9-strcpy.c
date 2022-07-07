0bd84284befb2dd8e4a781c19b4bf8961b37c388
root <5051@holbertonschool.com>
2022-07-06 23:48:47 -0700

    Add comments in function and update function.

--- 9-strcpy.c
+++ 9-strcpy.c
@@ -13,12 +13,17 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i] != 0)
+	/**
+	 * copies string src to dest while looking for
+	 * the null terminator
+	 */
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i = i + 1;
 	}
-	dest[i] = 0;
+	/* sets the end of dest to be null terminator */
+	dest[i] = '\0';
 
 	return (dest);
 }
