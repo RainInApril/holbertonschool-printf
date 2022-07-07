f76f8ce95185a6cfecc0287d79c32c36df36f263
root <km@rtin.mobi>
2022-07-07 03:39:55 -0700

    adding the main.h

--- main.h
+++ main.h
@@ -1,8 +1,26 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-typedef struct printf
-
-char *print_i(*format);
-
-#endif /* _MAIN_H_ */
+#ifndef __MAIN_H__							
+#define __MAIN_H__							
+							
+#include <stdio.h>							
+#include <stdlib.h>							
+#include <stdarg.h>							
+/**							
+* struct func - function							
+* @c: param							
+* @f: param							
+*/							
+							
+typedef struct func							
+{							
+char *c;							
+int (*f)(va_list args);							
+} struc;							
+							
+int ptr_char(va_list args);							
+int ptr_int(va_list args);							
+int _printf(const char *format, ...);							
+int _putchar(char c);							
+int funct_print(char j, va_list args);
+int print_s(va_list list);
+							
+#endif							
