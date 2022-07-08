e77eb22c2a1145b6a6b123303e5f381607df9f2e
root <chaubnphan@gmail.com>
2022-07-07 17:31:03 -0700

    Create a function to print char

--- print_char.c
+++ print_char.c
@@ -2,3 +2,30 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * print_char - prints the char to stdout
+ * @c: the char to be rpinted
+ *
+ * Return: 1
+ */
+
+int print_char(va_list list)
+{
+	unsigned char c;
+
+	c = va_arg(list, int);
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_percent - prints the %
+ *
+ * Return: 1
+ */
+
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
