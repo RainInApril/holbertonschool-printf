33f5e0fa1df639abefa740940171a4386a71acae
root <5051@holbertonschool.com>
2022-07-08 17:50:30 -0700

    Remove "{}" around if.

--- _printf.c
+++ _printf.c
@@ -50,9 +50,7 @@ int _printf(const char *format, ...)
 	va_start(list, format);
 
 	if (format == NULL)
-	{
 		return (1);
-	}
 	while (format != NULL && (format[index] != '\0'))
 	{
 		if (format[index] == '%')
